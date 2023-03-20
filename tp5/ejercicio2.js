93e2713514c4b3442ff30433f0fc8f2cc0d7375e
Gonzalo Fabre <gonzalo.fabre@outlook.es>
2023-03-20 16:00:00 -0300

    ejercicio 2 completo

--- tp5/ejercicio2.js
+++ tp5/ejercicio2.js
@@ -2,9 +2,12 @@ const vocales = "aáeéiíoóuú"
 
 const esVocal = (letra) => {
     if(typeof(letra) === "number") { 
-        let num = letra
-        return vocales.includes(num.toString())
+        // let num = letra
+        return vocales.includes(letra.toString())
     } else { 
        return vocales.includes(letra.toLowerCase());
     };
 };
+console.log(esVocal(5));
+
+console.log(esVocal("Á"))
