1925a4926144ada00ea5ef8a17637e48e8f4cdd4
Gonzalo Fabre <gonzalo.fabre@outlook.es>
2023-08-30 17:37:23 -0300

    Agregue filtros

--- Sprint-V/Frontend/src/App.jsx
+++ Sprint-V/Frontend/src/App.jsx
@@ -8,6 +8,16 @@ import peek from './utils/peek'
 function App() {
   const [products, setProducts] = useState([])
 
+  //Filters
+
+  const [filterByName, setFilterByName] = useState("");
+  const [filterByMinPrice, setFilterByMinPrice] = useState(0);
+  const [filterByMaxPrice, setFilterByMaxPrice] = useState(Infinity);
+
+  const categories = products.map((product) => product.category)
+  const uniqueCategories = [...new Set(categories)]
+  const [filterByCategory, setFilterByCategory] = useState("-")
+
   useEffect(() => {
     fetch(peek("http://localhost:3000/products"))
     .then((data) => peek(data.json()))
@@ -21,9 +31,55 @@ function App() {
   return (
     <>
     <div className='root'>
+    <div className="filters">
+        <h3>Buscar por :</h3>
+
+       <div className='filters-input'> <label htmlFor="filter-name">Nombre</label>
+        <input type="text" id="name" name="id"
+        onChange={(e) => setFilterByName(e.target.value)}
+        defaultValue={filterByName}/>
+        </div>
+
+        <br />
+        <div className='filters-input'>
+        <label htmlFor="filter-min-price">Precio mínimo</label>
+        <input type="number" name="filter-min-price" id="filter-min-price"
+        onChange={(e) => setFilterByMinPrice(e.target.value)}
+        defaultValue={filterByMinPrice} />
+        </div>
+
+        <br />
+        <div className='filters-input'>
+        <label htmlFor="filter-max-price">Precio máximo</label>
+        <input type="number" name="filter-max-price" id="filter-max-price"
+        onChange={(e) =>setFilterByMaxPrice(e.target.value)}/>
+        </div>
+
+        <br />
+        <div className='filters-input'>
+        <label htmlFor="filter-category">Categoría</label>
+        <select name="filter-category" id="filter-category"
+        onChange={(e) => setFilterByCategory(e.target.value)}>
+          <option value="-">-</option>
+          {
+            uniqueCategories.map((category, i) => (
+              <option key={`key-option-${category}${i}`} value={category}>{category}</option>
+            ))
+          }
+        </select>
+        </div>
+        
+
+
+    </div  >
     <div className='products-layout'>
     {
-    products.map((product) => (
+    products
+    .filter((product) => product.title.toLowerCase().includes(filterByName.toLowerCase()))
+    .filter((product) => product.price > filterByMinPrice)
+    .filter((product) => filterByMaxPrice!=0 ? product.price < filterByMaxPrice : product)
+    .filter((product) => filterByCategory === "-" ? true : product.category === filterByCategory)
+    .map((product) => (
       <Card 
       key={`key-${product.title}-${product.price}`}
     title={product.title}
---
+++ Sprint-V/Frontend/src/components/Filters.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Filters = () => {
+    return
+        <h3> Buscar por</h3>
+        <input></input>
+}
