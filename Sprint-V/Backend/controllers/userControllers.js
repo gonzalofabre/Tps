f3cfc5edf38810a284aacdd86ce62e1ec7c539d5
Gonzalo Fabre <gonzalo.fabre@outlook.es>
2023-09-07 00:51:04 -0300

    Login terminado

--- Sprint-V/Backend/controllers/userControllers.js
+++ Sprint-V/Backend/controllers/userControllers.js
@@ -1,5 +1,5 @@
-const { error } = require('console');
-const Users = require('../models/users');
+const { error } = require("console");
+const Users = require("../models/users");
 module.exports = {
   register: async (req, res) => {
     const { userName, password, tel, adress } = req.body;
@@ -14,19 +14,58 @@ module.exports = {
     try {
       const { userName, password } = req.body;
       const userFound = await Users.findOne({ where: { userName: userName } });
-  
+
       if (!userFound) {
-        return res.status(404).json({ status: 404, message: "Usuario no encontrado" });
+        return res
+          .status(404)
+          .json({ status: 404, message: "Usuario no encontrado" });
       }
-  
+
       if (userFound.password === password) {
-        return res.json({ data: { status: 200, message: "Usuario logeado", rol: userFound.rol } });
+        return userFound.rol === "admin"
+          ? res.json({
+              data: {
+                status: 200,
+                message: "Admin logeado",
+                userData: {
+                  id: userFound.id,
+                  userName: userFound.userName,
+                  name: userFound.name,
+                  lastName: userFound.lastName,
+                  password: userFound.password,
+                  adress: userFound.adress,
+                  tel: userFound.tel,
+                  rol: userFound.rol,
+                },
+              },
+            })
+          : res.json({
+              data: {
+                status: 200,
+                message: "Usuario logeado",
+                rol: userFound.rol,
+                userData: {
+                  id: userFound.id,
+                  userName: userFound.userName,
+                  name: userFound.name,
+                  lastName: userFound.lastName,
+                  password: userFound.password,
+                  adress: userFound.adress,
+                  tel: userFound.tel,
+                  rol: userFound.rol,
+                },
+              },
+            });
       } else {
-        return res.status(401).json({ status: 401, message: "Credenciales incorrectas" });
+        return res
+          .status(401)
+          .json({ status: 401, message: "Credenciales incorrectas" });
       }
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ status: 500, message: "Error interno del servidor" });
+      return res
+        .status(500)
+        .json({ status: 500, message: "Error interno del servidor" });
     }
   },
 };
--- Sprint-V/Frontend/src/components/Login/Login.jsx
+++ Sprint-V/Frontend/src/components/Login/Login.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Header from "../Header/Header";
 // import peek from "../utils/peek";
-import axios from 'axios';
+import axios from "axios";
+// import { redirect } from "react-router-dom";
 
 function Login() {
   const [isLoading, setIsLoading] = useState(false);
@@ -9,33 +10,63 @@ function Login() {
   const [password, setPassword] = useState("");
   return (
     <>
-    <Header/>
+      <Header />
 
-    <div className="login">
+      <div className="login">
         <label htmlFor="userName">Nombre de usuario</label>
-      <input value={userName} type="text" id="userName" onChange={(e) => setUserName(e.target.value)} />
-      <label htmlFor="password">Contraseña </label>
-      <input value={password} type="password" onChange={(e) => setPassword(e.target.value)} />
-      <button
-        onClick={async () => {
-          setIsLoading(true);
-          const data = {
-            userName, 
-            password
-          };
-          try {
-            const response = await axios.post("/api/users", data)
-            console.log(response.status)
-            console.log(response);
-          } catch (error) {
-            console.error(error.response.status);
-          }
-          setIsLoading(false);
-        }}
-      >
-        Login!
-      </button>
-    </div>
+        <input
+          value={userName}
+          type="text"
+          id="userName"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <label htmlFor="password">Contraseña </label>
+        <input
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            setIsLoading(true);
+            const data = {
+              userName,
+              password,
+            };
+            try {
+              const response = await axios.post("/api/users", data);
+              console.log(response.status);
+              console.log(response.data.data.userData);
+              const {
+                id,
+                adress,
+                tel,
+                password,
+                userName,
+                name,
+                lastName,
+                rol,
+              } = response.data.data.userData;
+              console.log(
+                id,
+                adress,
+                tel,
+                password,
+                userName,
+                name,
+                lastName,
+                rol
+              );
+
+            } catch (error) {
+              console.error(error.response.status);
+            }
+            setIsLoading(false);
+          }}
+        >
+          Login!
+        </button>
+      </div>
     </>
   );
 }
